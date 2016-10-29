6b251717568b65c1d2e3d369f203d84708fb514c
Cheng Ly <chengsieuly@gmail.com>
2016-10-28 19:02:50 -0700

    feat: Pass socket object to children

--- lib/SocketProvider.jsx
+++ lib/SocketProvider.jsx
@@ -2,17 +2,32 @@ import React, { Component, PropTypes } from 'react';
 import io from 'socket.io-client';
 
 class SocketProvider extends Component {
+    /* 
+     * NOTE: 'context' is an experimental feature of react
+     * and may be changed in future releases
+     * https://facebook.github.io/react/docs/context.html
+     **/
+    getChildContext() {
+        return {
+            socket: this.props.socket
+        };
+    }
+
     render() {
         return this.props.children;
     }
 }
 
 SocketProvider.propTypes = {
-    socket: PropTypes.object.isRequired 
+    socket: PropTypes.object.isRequired
 };
 
-SocketProvider.defaultProps = { 
-    socket: io() 
+SocketProvider.childContextTypes = {
+    socket: PropTypes.object.isRequired
+};
+
+SocketProvider.defaultProps = {
+    socket: io()
 };
 
 export default SocketProvider;
